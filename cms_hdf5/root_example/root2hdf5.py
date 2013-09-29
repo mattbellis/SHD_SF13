2286c7e04a2cf274e4724f27e706153a75e2d12b
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 12:15:32 -0400

    try just NJet

--- cms_hdf5/root_example/root2hdf5.py
+++ cms_hdf5/root_example/root2hdf5.py
@@ -3,10 +3,24 @@ import ROOT
 import h5py
 
 def root2hdf5(infile, outfile):
-    tree = infile.Get("data")
+    outgroup = outfile.create_group('data')
+    intree = infile.Get('data')
 
-if __name__ == '__main__':
+    outdataset = outgroup.create_dataset('NJet', (intree.GetEntries(), intree.NJet, 5))
+    for i in xrange(intree.GetEntries()):
+        j = intree.GetEntry(i)
+        for j in xrange(intree.NJet):
+            outdataset[i,j,0] = intree.Jet_E[j]
+            outdataset[i,j,1] = intree.Jet_Px[j]
+            outdataset[i,j,2] = tree.Jet_Py[j]
+            outdataset[i,j,3] = tree.Jet_Pz[j]
+            outdataset[i,j,4] = tree.Jet_btag[j]
+
+def main():
     import sys
     infile = ROOT.TFile(sys.argv[1])
     outfile = h5py.File(sys.argv[2], 'w')
     root2hdf5(infile, outfile)
+
+if __name__ == '__main__':
+    main()
