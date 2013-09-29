7c2e275adc3bb0e6447d5d400640e118e4c6683f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 12:35:09 -0400

    oeu

--- cms_hdf5/root_example/root2hdf5.py
+++ cms_hdf5/root_example/root2hdf5.py
@@ -2,25 +2,24 @@ import numpy as np
 import ROOT
 import h5py
 
-def root2hdf5(infile, outfile):
-    outgroup = outfile.create_group('data')
-    intree = infile.Get('data')
+def root2hdf5(in_file, out_file):
+    out_group = out_file.create_group('data')
+    in_tree = in_file.Get('data')
 
-    outdataset = outgroup.create_dataset('NJet', (intree.GetEntries(), intree.NJet, 5))
-    for i in xrange(intree.GetEntries()):
-        j = intree.GetEntry(i)
-        for j in xrange(intree.NJet):
-            outdataset[i,j,0] = intree.Jet_E[j]
-            outdataset[i,j,1] = intree.Jet_Px[j]
-            outdataset[i,j,2] = tree.Jet_Py[j]
-            outdataset[i,j,3] = tree.Jet_Pz[j]
-            outdataset[i,j,4] = tree.Jet_btag[j]
+    for i in xrange(in_tree.GetEntries()):
+        out_dataset = out_group.create_dataset(str(i), (in_tree.GetEntry(i), in_tree.NJet, 5))
+        for j in xrange(in_tree.GetEntry(i)):
+            out_dataset[i,j,0] = in_tree.Jet_E[j]
+            out_dataset[i,j,1] = in_tree.Jet_Px[j]
+            out_dataset[i,j,2] = in_tree.Jet_Py[j]
+            out_dataset[i,j,3] = in_tree.Jet_Pz[j]
+            out_dataset[i,j,4] = in_tree.Jet_btag[j]
 
 def main():
     import sys
-    infile = ROOT.TFile(sys.argv[1])
-    outfile = h5py.File(sys.argv[2], 'w')
-    root2hdf5(infile, outfile)
+    in_file = ROOT.TFile(sys.argv[1])
+    out_file = h5py.File(sys.argv[2], 'w')
+    root2hdf5(in_file, out_file)
 
 if __name__ == '__main__':
     main()
